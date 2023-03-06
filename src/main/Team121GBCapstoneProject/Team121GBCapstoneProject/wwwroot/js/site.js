b998a39e77d57fa17eac9ca35a103dce0123c151
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-06 06:49:07 -0800

    NathanieL: addes a modal for user list names.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/site.js
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/site.js
@@ -24,11 +24,10 @@ $(document).ready(function () {
                                     <td><img src="${resizedCoverArt}"></td>
                                     <td id="td${i}">${game.gameTitle}</td>
                                     <td><a href="${game.gameWebsite}">${game.gameWebsite}"</a></td>
-                                    <td><button id="${i}" class="btn btn-primary">Add Game</button></td>
+                                    <td><button id="${i}" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#AddGame">Add Game</button></td>
                                </tr>`;
                     $("#gameTableBody").append(row);
                 });
-
                 // redirect to the search results page after the ajax call is successful
                 //window.location.href = "/Search/Results";
             }
@@ -37,19 +36,34 @@ $(document).ready(function () {
     //put the get the lists method
     $("table").on("click", "button", function () {
         let id = $(this).attr("id");
-        alert(id);
         let tableDataId = $(`#td${id}`).attr("id");
-        alert(tableDataId)
-        //const title = document.getElementById("tableDataId");
-        //console.log(id);
-        //console.log(tableDataId);
-        //console.log(title);
+        const title = $(`#${tableDataId}`).text();
+        console.log(id);
+        console.log(tableDataId);
+        console.log(title);
+        $.ajax({
+            type: "GET",
+            url: "/api/Game/getUserLists",
+            dataType: "json",
+            success: function (data) {
+                //$.each(data, function (index, value) {
+                //    console.log(index + ": " + value.val());
+                //});
+                for (let i = 0; i < data.length; ++i) {
+                    console.log(data[i]);
+                }
+
+            },
+            error: console.log("You don't have any lists to add a game to!")
+        });
+
+
         //$.ajax({
         //    type: "POST",
         //    dataType: "json",
         //    url: `api/Game/addGame`,
         //    contentType: "application/json; charset=UTF-8",
-        //    data: JSON.stringify(),
+        //    data: JSON.stringify(title),
         //    success: afterAddGame,
         //    error: errorAlert
         //});
@@ -60,10 +74,11 @@ function afterAddGame() {
     $("#confirmation").empty();
     let confirmation = `<h1 class="text-success">
                             Success!
-                        </h1>`
+                        </h1>`;
+    $("#confirmation").append(confirmation);
 }
 
-    function errorAlert() {
-        alert("Something went wrong, please try again.")
-    }
+function errorAlert() {
+    alert("Something went wrong, please try again.")
+}
 
